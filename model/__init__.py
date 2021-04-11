193edd722322a1fdefd892887eb9ab48223f2a1e
Rasul Karimov <yoatom96@hotmail.com>
2021-04-12 01:05:06 +0300

    add iresnet and partial FC layer

--- model/__init__.py
+++ model/__init__.py
@@ -2,6 +2,8 @@ from .base import FaceModule
 from .losses import MLSLoss, AngleLoss
 from .spherenet import SphereNet20
 from .heads import PFEHead
+from .iresnet import iresnet18, iresnet34, iresnet50, iresnet100
+from .partial_fc import PartialFC
 
 model_dict = {
     "spherenet20": SphereNet20,
