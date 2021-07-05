8b49510741637c8d8fd296ab288ebcef1a903dc0
remiruzn <remiru07@gmail.com>
2021-07-05 22:45:02 +0300

    changes typo

--- trainers/train_pfe.py
+++ trainers/train_pfe.py
@@ -169,7 +169,7 @@ class Trainer(TrainerBase):
 
             feature, sig_feat = self.backbone(img)
 
-            print(sig_feat)
+            print(sig_feat.view(sig_feat.size(0), -1))
             sig_feat_dict = {"bottleneck_feature" :  sig_feat}
 
             log_sig_sq = self.head(sig_feat_dict)
