45c2417765e7d50dd1414fb1e8f33602988a0577
romakail <roman.kail@mail.ru>
2021-07-31 20:43:10 +0300

    Developing rejected verificaton
    - The core pipeline works and gives good results, but I have to add a lot of details

--- face_lib/utils/reject_verification.py
+++ face_lib/utils/reject_verification.py
@@ -4,6 +4,8 @@ import argparse
 import numpy as np
 import torch
 from path import Path
+from tqdm import tqdm
+from collections import defaultdict
 
 # path = str(
 #     Path(Path(Path(__file__).parent.abspath()).parent.abspath()).parent.abspath()
@@ -15,13 +17,30 @@ from face_lib.datasets import IJBDataset, IJBATest, IJBCTest
 from face_lib import models as mlib, utils
 from face_lib.utils import cfg
 from face_lib.utils.imageprocessing import preprocess
-from face_lib.utils.fusion import extract_features
+from face_lib.utils.fusion import extract_features, force_compare
 from face_lib.utils.fusion_metrics import (
     pair_euc_score,
     pair_cosine_score,
     pair_MLS_score,
 )
+from face_lib.utils.utils import harmonic_mean
+import face_lib.utils.fusion_metrics as metrics
 from face_lib.utils.fusion_metrics import l2_normalize, aggregate_PFE
+import matplotlib.pyplot as plt
+
+
+def calculate_uncertainties(mu_1, mu_2, sigma_sq_1, sigma_sq_2):
+    return harmonic_mean(sigma_sq_1, axis=1) + harmonic_mean(sigma_sq_2, axis=1)
+
+
+def plot_rejected_TAR_FAR(table, rejected_portions, save_fig_path=None):
+    fig, ax = plt.subplots()
+    for far, tars in table.items():
+        ax.plot(rejected_portions, tars, label="TAR@FAR=" + str(far), marker=".")
+    fig.legend()
+    if save_fig_path:
+        fig.savefig(save_fig_path)
+    return fig
 
 
 def eval_reject_verification(
@@ -30,18 +49,21 @@ def eval_reject_verification(
     dataset_path,
     pairs_table_path,
     batch_size=64,
-    rejected_portions=None):
+    rejected_portions=None,
+    FARs=None):
 
     if rejected_portions is None:
         rejected_portions = [0., ]
+    if FARs is None:
+        FARs = [0.]
 
-    pairs, labels = [], []
+    pairs, label_vec = [], []
     unique_imgs = set()
     with open(pairs_table_path, "r") as f:
         for line in f.readlines():
             left_path, right_path, label = line.split(",")
             pairs.append((left_path, right_path))
-            labels.append(int(label))
+            label_vec.append(int(label))
             unique_imgs.add(left_path)
             unique_imgs.add(right_path)
 
@@ -64,17 +86,38 @@ def eval_reject_verification(
     mu_2 = np.array([mu[img_to_idx[pair[1]]] for pair in pairs])
     sigma_sq_1 = np.array([sigma_sq[img_to_idx[pair[0]]] for pair in pairs])
     sigma_sq_2 = np.array([sigma_sq[img_to_idx[pair[1]]] for pair in pairs])
-    labels = np.array(labels, dtype=bool)
+    label_vec = np.array(label_vec, dtype=bool)
 
     print("Mu_1 :", mu_1.shape, mu_1.dtype)
     print("Mu_2 :", mu_2.shape, mu_2.dtype)
     print("sigma_sq_1 :", sigma_sq_1.shape, sigma_sq_1.dtype)
     print("sigma_sq_2 :", sigma_sq_2.shape, sigma_sq_2.dtype)
-    print("labels :",  labels.shape,  labels.dtype)
-    print(labels.dtype)
+    print("labels :",  label_vec.shape,  label_vec.dtype)
 
-    # score_vec = compare_func(features1, features2)
-    print("Hello world")
+    score_vec = force_compare(pair_cosine_score)(mu_1, mu_2, sigma_sq_1, sigma_sq_2)
+    uncertainty_vec = calculate_uncertainties(mu_1, mu_2, sigma_sq_1, sigma_sq_2)
+
+    sorted_indices = uncertainty_vec.argsort()
+    score_vec = score_vec[sorted_indices]
+    label_vec = label_vec[sorted_indices]
+    assert score_vec.shape == label_vec.shape
+
+    result_table = defaultdict(list)
+    result_fars = defaultdict(list)
+    for rejected_portion in tqdm(rejected_portions):
+        cur_len = int(score_vec.shape[0] * (1 - rejected_portion))
+        tars, fars, thresholds = metrics.ROC(score_vec[:cur_len], label_vec[:cur_len], FARs=FARs)
+        for far, tar in zip(FARs, tars):
+            result_table[far].append(tar)
+        for wanted_far, real_far in zip(FARs, fars):
+            result_fars[wanted_far].append(real_far)
+
+    print("Result fars :")
+    print(result_fars)
+    print("TAR@FARs :")
+    print(result_table)
+
+    return result_table
 
 
 if __name__ == "__main__":
@@ -104,6 +147,10 @@ if __name__ == "__main__":
         help="Portion of rejected pairs of images",
         nargs="+", )
     parser.add_argument(
+        "--FARs",
+        help="Portion of rejected pairs of images",
+        nargs="+", )
+    parser.add_argument(
         "--figure_path",
         help="The figure will be saved to this path",
         type=str, default=None, )
@@ -111,6 +158,10 @@ if __name__ == "__main__":
         "--device_id",
         help="Gpu id on which the algorithm will be launched",
         type=int, default=0, )
+    parser.add_argument(
+        "--save_fig_path",
+        help="Path to save figure to",
+        type=str, default=None, )
 
     args = parser.parse_args()
 
@@ -130,11 +181,16 @@ if __name__ == "__main__":
     head.load_state_dict(checkpoint["uncertain"])
 
     rejected_portions = list(map(float, args.rejected_portions))
+    FARs = list(map(float, args.FARs))
 
-    eval_reject_verification(
+    result_table = eval_reject_verification(
         backbone,
         head,
         args.dataset_path,
         args.pairs_table_path,
         batch_size=args.batch_size,
-        rejected_portions=rejected_portions)
+        rejected_portions=rejected_portions,
+        FARs=FARs)
+
+    if args.save_fig_path:
+        plot_rejected_TAR_FAR(result_table, rejected_portions, args.save_fig_path)
--- scripts/eval_filtering_out.sh
+++ scripts/eval_filtering_out.sh
@@ -1,8 +1,10 @@
 python3 ./face_lib/utils/reject_verification.py \
   --checkpoint_path=/gpfs/gpfs0/r.karimov/models/pfe/first_ms1m_pfe/sota.pt \
-  --dataset_path=/gpfs/gpfs0/r.karimov/IJB/aligned_data_for_fusion/debug \
-  --pairs_table_path=/gpfs/gpfs0/r.karimov/IJB/aligned_data_for_fusion/metadata_refuse_verification/pairs_100_prob_0.5_debug.csv \
+  --dataset_path=/gpfs/gpfs0/r.karimov/IJB/aligned_data_for_fusion/big \
+  --pairs_table_path=/gpfs/gpfs0/r.karimov/IJB/aligned_data_for_fusion/metadata_refuse_verification/pairs_10000000_prob_0.5.csv \
   --config_path=./configs/models/iresnet_ms1m_pfe.yaml \
   --batch_size=64 \
-  --rejected_portions 0.0001 0.001 0.01 0.1 0.2 0.5 \
-  --device_id=0
+  --FARs 0.0001 0.0005 0.001 0.005 0.01 0.05 \
+  --rejected_portions 0.005 0.01 0.02 0.05 0.1 0.2 \
+  --device_id=0 \
+  --save_fig_path=/gpfs/gpfs0/r.kail/figures/rejected.png
