8d87edf5e83442dc3a7f126101153d3c507ddd29
romakail <roman.kail@mail.ru>
2021-08-10 16:51:55 +0300

    Typo fix

--- scripts/eval_fusion_ijbc.sh
+++ scripts/eval_fusion_ijbc.sh
@@ -18,12 +18,12 @@ python3 ./face_lib/utils/fusion.py \
 #  --checkpoint_path=/gpfs/gpfs0/r.zainulin/sota.pth \
 #  --dataset_path=/gpfs/gpfs0/r.karimov/IJB/aligned_data_for_fusion/big \
 #  --protocol_path=/gpfs/gpfs0/r.karimov/IJB/IJB-C/protocols/archive \
-#  --config_path=./configs/models/iresnet_ms1m_pfe.yaml \
+#  --config_path=./configs/models/iresnet_ms1m_probface.yaml \
 #  --uncertainty_strategy=head \
 #  --protocol=ijbc \
 #  --fusion_distance_methods random_cosine mean_cosine PFE_cosine \
 #  --FARs 1e-5 5e-5 1e-4 5e-4 1e-3 5e-3 1e-2 5e-2 \
 #  --device_id=0 \
 #  --batch_size=64 \
-#  --save_table_path=/gpfs/gpfs0/r.kail/tables/res_probface.pkl \
+#  --save_table_path=/gpfs/gpfs0/r.kail/tables/res_probface_good.pkl \
 #  --verbose
