393a4d8c53c86bf53b2fe40266c6305004407d28
romakail <roman.kail@mail.ru>
2021-10-11 19:44:59 +0300

    New scripts

--- scripts/eval_reject_verification.sh
+++ scripts/eval_reject_verification.sh
@@ -59,7 +59,7 @@
 #python3 ./face_lib/utils/reject_verification.py \
 #  --checkpoint_path=/gpfs/data/gpfs0/k.fedyanin/space/models/pair_classifiers/first_pair_classifier_advanced.pth \
 #  --dataset_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/big \
-#  --pairs_table_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/metadata_refuse_verification/pairs_1000000_prob_0.5.csv \
+#  --pairs_table_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/metadata_refuse_verification/pairs_1000_prob_0.5.csv \
 #  --config_path=./configs/models/iresnet_ms1m_face_classification.yaml \
 #  --batch_size=64 \
 #  --distaces_batch_size=100 \
@@ -68,11 +68,11 @@
 #  --rejected_portions $(seq 0 0.002 0.2) \
 #  --distance_uncertainty_metrics classifier_classifier cosine_classifier MLS_classifier cosine_harmonic-sum MLS_harmonic-sum \
 #  --device_id=0 \
-#  --save_fig_path=/gpfs/data/gpfs0/r.kail/figures/classifier_1/
+#  --save_fig_path=/gpfs/data/gpfs0/r.kail/figures/classifiers/classifier_1/
 
-## PFE normalized
+### PFE spectral
 #python3 ./face_lib/utils/reject_verification.py \
-#  --checkpoint_path=/gpfs/data/gpfs0/k.fedyanin/space/models/pfe/spectral/head_simple_vs_spectral/simple_head/checkpoints/sota.pth \
+#  --checkpoint_path=/gpfs/data/gpfs0/k.fedyanin/space/models/pfe/spectral_normalized_pfe/sota.pth \
 #  --dataset_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/big \
 #  --pairs_table_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/metadata_refuse_verification/pairs_1000000_prob_0.5.csv \
 #  --config_path=./configs/models/iresnet_ms1m_pfe_normalized_spectral.yaml \
@@ -82,18 +82,4 @@
 #  --rejected_portions $(seq 0 0.002 0.2) \
 #  --distance_uncertainty_metrics cosine_mean cosine_harmonic-sum cosine_harmonic-harmonic MLS_harmonic-sum MLS_harmonic-harmonic \
 #  --device_id=0 \
-#  --save_fig_path=/gpfs/data/gpfs0/r.kail/figures/heads_comparison_simple/
-
-# PFE normalized
-python3 ./face_lib/utils/reject_verification.py \
-  --checkpoint_path=/gpfs/data/gpfs0/k.fedyanin/space/models/pfe/spectral/head_simple_vs_spectral/spectral_head/checkpoints/sota.pth \
-  --dataset_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/big \
-  --pairs_table_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/metadata_refuse_verification/pairs_1000000_prob_0.5.csv \
-  --config_path=./configs/models/iresnet_ms1m_pfe_normalized_spectralhead.yaml \
-  --batch_size=64 \
-  --uncertainty_strategy=head \
-  --FARs 0.0001 0.0005 0.001 0.005 0.01 0.05 \
-  --rejected_portions $(seq 0 0.002 0.2) \
-  --distance_uncertainty_metrics cosine_mean cosine_harmonic-sum cosine_harmonic-harmonic MLS_harmonic-sum MLS_harmonic-harmonic \
-  --device_id=0 \
-  --save_fig_path=/gpfs/data/gpfs0/r.kail/figures/heads_comparison_spectral/
+#  --save_fig_path=/gpfs/data/gpfs0/r.kail/figures/spectral/
