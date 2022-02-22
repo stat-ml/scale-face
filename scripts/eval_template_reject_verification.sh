98082ee002aa26404a8a897639246d02a07b79ac
romakail <roman.kail@mail.ru>
2022-02-22 12:53:45 +0300

    Improve template_reject_verification_script

--- scripts/eval_template_reject_verification.sh
+++ scripts/eval_template_reject_verification.sh
@@ -1,4 +1,20 @@
-# Scale
+## Scale
+#python3 ./face_lib/evaluation/template_reject_verification.py \
+#  --checkpoint_path=/gpfs/gpfs0/k.fedyanin/space/models/scale/01_frozen/01_sigm_mul/checkpoint.pth \
+#  --dataset_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/small \
+#  --protocol=ijbc \
+#  --protocol_path=/gpfs/gpfs0/k.fedyanin/space/IJB/IJB-C/protocols/archive \
+#  --config_path=./configs/scale/sigm_mul.yaml \
+#  --batch_size=64 \
+#  --uncertainty_strategy=scale \
+#  --uncertainty_mode=confidence \
+#  --FARs 0.0001 0.0005 0.001 0.005 0.01 0.05 \
+#  --rejected_portions $(seq 0 0.002 0.5) \
+#  --fusion_distance_uncertainty_metrics mean_cosine_mean mean_cosine_harmonic-harmonic mean_cosine_mul \
+#  --device_id=0 \
+#  --save_fig_path=/beegfs/home/r.kail/faces/figures/test
+
+# Scale with batching
 python3 ./face_lib/evaluation/template_reject_verification.py \
   --checkpoint_path=/gpfs/gpfs0/k.fedyanin/space/models/scale/01_frozen/01_sigm_mul/checkpoint.pth \
   --dataset_path=/gpfs/gpfs0/k.fedyanin/space/IJB/aligned_data_for_fusion/small \
@@ -6,6 +22,7 @@ python3 ./face_lib/evaluation/template_reject_verification.py \
   --protocol_path=/gpfs/gpfs0/k.fedyanin/space/IJB/IJB-C/protocols/archive \
   --config_path=./configs/scale/sigm_mul.yaml \
   --batch_size=64 \
+  --distaces_batch_size=8 \
   --uncertainty_strategy=scale \
   --uncertainty_mode=confidence \
   --FARs 0.0001 0.0005 0.001 0.005 0.01 0.05 \
