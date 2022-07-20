3d8e558ecc4cb39a7c936151a0c6bce09ead47fd
Kirill Fedyanin <kirill.fedyanin@gmail.com>
2022-07-20 17:15:23 +0400

    Scale is working!

--- explore/cross.py
+++ explore/cross.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, precision_recall_curve
 
 sys.path.append(".")
-# from iresnet import iresnet50
 from face_lib.models.iresnet import iresnet50
 from face_lib.utils.imageprocessing import preprocess
 from face_lib.evaluation.feature_extractors import extract_features_uncertainties_from_list
@@ -75,29 +74,52 @@ def precalculate_images(model, image_paths, batch_size=20):
 def main():
     model = load_model()
     df = pd.read_csv(cplfw_dir / 'pairs.csv')
-    cut = 200
+    cut = 300
     # df = pd.concat([df.iloc[:cut], df.iloc[-cut:]])
     lst = np.unique(df.photo_1.to_list() + df.photo_2.to_list())
-    cached = precalculate_images(model, lst)
+    cached = precalculate_images(model, lst, batch_size=100)
 
+    ue = {}
     for path, pred in cached.items():
+        ue[path] = torch.norm(pred).item()
         cached[path] = pred / torch.norm(pred)
 
     def check(row):
         x_0, x_1 = cached[row['photo_1']], cached[row['photo_2']]
         return (x_0 * x_1).sum().item()
 
+    def basic_ue(row):
+        x_0, x_1 = ue[row['photo_1']], ue[row['photo_2']]
+        # return x_1
+        return np.sqrt(x_0*x_1)
+
     scores = df.apply(check, axis=1)
+    ues = np.array(df.apply(basic_ue, axis=1))
     labels = list(df.label)
+
+    print(ues)
 
     preds = (scores > 0.19).astype(np.uint)
     print((preds == labels).mean())
-    print(len(preds))
 
-    precisions, recalls, threshs = precision_recall_curve(labels, scores)
-    plt.plot(threshs, precisions[1:])
-    plt.plot(threshs, recalls[1:])
+    correct = np.array((preds == labels))
+    idxs = np.argsort(ues)
+    splits = np.arange(0, 1, 0.1)
+    accuracies = []
+    for split in splits:
+        i = int(len(correct) * split)
+        accuracies.append(correct[idxs[i:]].mean())
+
+    plt.plot(splits, accuracies)
     plt.show()
+
+
+    # print(len(preds))
+    #
+    # precisions, recalls, threshs = precision_recall_curve(labels, scores)
+    # plt.plot(threshs, precisions[1:])
+    # plt.plot(threshs, recalls[1:])
+    # plt.show()
 
 
 if __name__ == '__main__':
