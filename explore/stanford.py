6ef63e3b9a24b44076971cfb5bdaac8cc7e170c5
Kirill Fedyanin <kirill.fedyanin@gmail.com>
2022-08-15 12:23:53 +0400

    ffcv

--- explore/stanford.py
+++ explore/stanford.py
@@ -1,6 +1,6 @@
 """
-level 2:
-use tensorboard
+level 3:
+convert all to rgb
 """
 import os
 from pathlib import Path
@@ -14,9 +14,8 @@ from tqdm import tqdm
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, Dataset
-from torchvision.io import read_image, ImageReadMode
 from torch.utils.tensorboard import SummaryWriter
-
+import torchvision
 
 categories = [
     'bicycle', 'cabinet', 'chair', 'coffee_maker', 'fan', 'kettle',
@@ -39,6 +38,56 @@ def make_small(data_dir, small_dir):
         )
         img.save(small_dir / path)
 
+from ffcv.writer import DatasetWriter
+from ffcv.fields import RGBImageField, IntField, FloatField
+from ffcv.loader import Loader, OrderOption
+from ffcv.transforms import ToTensor, ToDevice, ToTorchImage, Cutout, Convert, Squeeze
+from ffcv.fields.decoders import IntDecoder, RandomResizedCropRGBImageDecoder, FloatDecoder, SimpleRGBImageDecoder
+from PIL import Image
+
+
+def convert_to_rgb(base_directory):
+    subdicrectories = os.listdir(base_directory)
+    for directory in subdicrectories:
+        print(directory)
+        files = os.listdir(base_directory / directory)
+        for file in tqdm(files):
+            filename = base_directory / directory / file
+            img = Image.open(filename)
+            if img.mode != 'RGB':
+                rgb_img = Image.new("RGB", img.size)
+                rgb_img.paste(img)
+                rgb_img.save(filename)
+
+
+def ffcv_loader_by_df(df, base_dir, write_path, random_order=False, batch_size=100):
+    image_paths = np.array(list(df.path))
+    labels = np.array(list(df.super_class_id)) - 1
+    dataset = DatasetS(base_dir, image_paths, labels)
+
+    writer = DatasetWriter(write_path, {
+        'image': RGBImageField(max_resolution=32),
+        'label': IntField()
+    })
+    writer.from_indexed_dataset(dataset)
+
+    image_pipeline = [SimpleRGBImageDecoder(), ToTensor(), ToDevice(0), ToTorchImage(), Convert(torch.float32), torchvision.transforms.Normalize(127., 50.)]
+    label_pipeline = [IntDecoder(), ToTensor(), ToDevice(0), Squeeze()]
+
+    # Pipeline for each data field
+    pipelines = {
+        'image': image_pipeline,
+        'label': label_pipeline
+    }
+
+    if random_order:
+        order = OrderOption.RANDOM
+    else:
+        order = OrderOption.SEQUENTIAL
+
+    # Replaces PyTorch data loader (`torch.utils.data.Dataloader`)
+    loader = Loader(write_path, batch_size=batch_size, num_workers=12, order=order, pipelines=pipelines)
+    return loader
 
 
 class Model(nn.Module):
@@ -46,13 +95,22 @@ class Model(nn.Module):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Linear(3072, 256),
+            nn.BatchNorm1d(256),
+            nn.ELU(),
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
+            nn.ELU(),
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
             nn.ELU(),
             nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
             nn.ELU(),
             nn.Linear(128, 2)
         )
 
     def forward(self, x):
+        x = x.reshape(-1, 3072)
         return self.layers(x)
 
 
@@ -66,8 +124,7 @@ class DatasetS(Dataset):
         return len(self.labels)
 
     def __getitem__(self, idx):
-        image = read_image(str(self.base_dir / self.image_paths[idx]), ImageReadMode.RGB)
-        image = ((image - 128) / 255).flatten()
+        image = Image.open(str(self.base_dir / self.image_paths[idx]))
         return image, self.labels[idx]
 
 
@@ -80,15 +137,21 @@ def loader_by_df(df, base_dir, batch_size):
 
 def main():
     data_dir = Path('/home/kirill/data/stanford/Stanford_Online_Products')
-    small_dir = Path('/home/kirill/data/stanford/small')
+    small_dir = Path('/home/kirill/data/stanford/small2')
+    # convert_to_rgb(small_dir)
 
     df = pd.read_csv(data_dir / 'Ebay_info.txt', delim_whitespace=True, index_col='image_id')
     df = df[df.super_class_id.isin([1, 2])]
-    idx = np.random.choice(range(len(df)), 5000)
-    df = df.iloc[idx]
+    idx = np.random.choice(range(len(df)), 5000, replace=False)
     split = 3000
-    train_loader = loader_by_df(df.iloc[:split], small_dir, batch_size=50)
-    val_loader = loader_by_df(df.iloc[split:], small_dir, batch_size=10000)
+    df = df.iloc[idx]
+
+    train_loader = ffcv_loader_by_df(df.iloc[:split], small_dir, '/tmp/ds_train.beton', random_order=True, batch_size=50)
+    val_df = df.iloc[split:]
+    val_loader = ffcv_loader_by_df(val_df, small_dir, '/tmp/ds_val.beton', random_order=False, batch_size=len(val_df))
+
+    # train_loader = loader_by_df(df.iloc[:split], small_dir, batch_size=50)
+    # val_loader = loader_by_df(df.iloc[split:], small_dir, batch_size=10000)
 
     model = Model().cuda()
     optimizer = torch.optim.SGD(model.parameters(), lr=3e-3)
