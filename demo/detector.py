a98c8c660aedbbe0c10170caa7b1242638442761
Kirill Fedyanin <kirill.fedyanin@gmail.com>
2022-08-30 17:32:48 +0400

    Barista

--- demo/detector.py
+++ demo/detector.py
@@ -22,24 +22,13 @@ src = np.array(
     ],
     dtype=np.float32,
 )
-src[:, 0] += 8.0  # this is for 112 x 112 pictures, comment this if you have 112 x 96
 
 face_parts = ("left_eye", "right_eye", "nose", "mouth_left", "mouth_right")
-
-
 
 
 def draw_box(image, b):
     image = cv2.rectangle(image, (b[0], b[1]), (b[2], b[3]), (36, 255, 12), 2)
     return image
-
-class Barista:
-    def __init__(self):
-        pass
-
-    def draw(self, image, confidence):
-        return image
-        pass
 
 def align_face(full_image, landmarks):
     tform = trans.SimilarityTransform()
@@ -64,9 +53,37 @@ class Detector:
         return box, confidence, landmarks
 
 
+class Barista:
+    def __init__(self):
+        self.confidence = 0
+
+    def __call__(self, image, confidence):
+        confidence = (confidence - 0.95) * 20
+        confidence = max(0, confidence)
+        alpha = 0.2
+        self.confidence = (1-alpha)*self.confidence + alpha*confidence
+        print(self.confidence)
+
+        bar_top = (600, int(470 - 400 * self.confidence))
+        color = self._color(self.confidence)
+        image = cv2.rectangle(image, bar_top, (620, 470), color, 20)
+        return image
+
+    def _color(self, confidence):
+        """confidence should b"""
+        assert confidence >= 0
+        assert  confidence < 1
+        green = 20 + 235 * confidence
+        red = 255 - 235 * confidence
+        return (36, green, red)
+
+
+
+
 class ImageProcessor:
     def __init__(self):
         self.detector = Detector()
+        self.barista = Barista()
 
     def __call__(self, frame):
         try:
@@ -76,7 +93,9 @@ class ImageProcessor:
                 print(face_aligned.shape)
 
                 frame = draw_box(frame, box)
+                frame = self.barista(frame, confidence)
                 cv2.imshow('frame', frame)
+                # cv2.imshow('frame', face_aligned)
         except IndexError:
             print('wtf')
 
