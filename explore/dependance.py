a6697e99f30ff9a1148083b20a3576d8753f2845
Kirill Fedyanin <kirill.fedyanin@gmail.com>
2022-08-30 12:13:45 +0400

    Cleaner

--- explore/dependance.py
+++ explore/dependance.py
@@ -1,5 +1,5 @@
 """
-How noise is depends on scale
+lvl 7: experiments motivation
 """
 
 import sys
@@ -22,27 +22,34 @@ def main():
     args.images_path = f'{dataset}/aligned images'
     args.short = False
     data_directory = Path(args.data_directory)
-    pairs = get_pairs(data_directory, dataset, short=True)
+    pairs = get_pairs(data_directory, dataset, short=args.short)
     photo_list = np.unique(pairs.photo_1.to_list() + pairs.photo_2.to_list())
 
-    preprocessor = Preprocessor(data_directory / args.images_path, is_training=False)
+    preprocessor = Preprocessor(data_directory / args.images_path, is_training=True)
     checkpoint_path = data_directory / args.checkpoint_path
     model = load_model(args.uncertainty_type, checkpoint_path)
 
     inferencer = Inferencer(preprocessor, model, 100)
     multi_mus = []
     multi_sigmas = []
-    for _ in range(2):
+    for _ in range(5):
         mus, sigmas = inferencer(photo_list)
         multi_mus.append(torch.stack(tuple(mus.values())))
         multi_sigmas.append(torch.tensor(tuple(sigmas.values())))
 
-    multi_mus = torch.stack(multi_mus)
-    multi_sigmas = torch.stack(multi_sigmas)
-    import ipdb; ipdb.set_trace()
+    multi_mus = torch.stack(multi_mus).numpy()
+    multi_sigmas = torch.stack(multi_sigmas).numpy()
 
-    # plt.hist(sigmas.values(), bins=100)
-    # plt.show()
+    sigmas = np.linalg.norm(np.std(multi_mus, axis=0), axis=-1)
+    mean_scale = np.mean(multi_sigmas, axis=0)
+
+    # mean_scale = mean_scale[sigmas < 0.1]
+    # sigmas = sigmas[sigmas < 0.1]
+
+    plt.scatter(mean_scale, sigmas, alpha=0.5, s=5)
+    plt.show()
+
+
 
 
 if __name__ == '__main__':
